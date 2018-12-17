5f35daecd97d74a71e1a1f281bda8d46db639120
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-17 02:50:47 +0100

    added bulma 0.7.2 and all extensions to the repo, and added necessary boilerplate

---
+++ bulma/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoSimpleBulmaConfig(AppConfig):
+    name = 'django_simple_bulma'
---
+++ bulma/finders.py
@@ -0,0 +1,24 @@
+from django.contrib.staticfiles.finders import BaseFinder
+
+
+class SimpleBulmaFinder(BaseFinder):
+    """
+    A custom Finder class to compile bulma to static files,
+    and then return paths to those static files so they may be collected
+    by the static collector.
+    """
+    def find(self, path, all=False):
+        """
+        Given a relative file path, find an absolute file path.
+
+        If the ``all`` parameter is False (default) return only the first found
+        file path; if True, return a list of all found files paths.
+        """
+        raise NotImplementedError('subclasses of BaseFinder must provide a find() method')
+
+    def list(self, ignore_patterns):
+        """
+        Given an optional list of paths to ignore, return a two item iterable
+        consisting of the relative path and storage instance.
+        """
+        raise NotImplementedError('subclasses of BaseFinder must provide a list() method')
