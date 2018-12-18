71e22b2b59c4a414c8aa2c0049c2e0dfc180f436
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-18 01:27:27 +0100

    More work on the finder.

--- bulma/finders.py
+++ bulma/finders.py
@@ -6,6 +6,8 @@ files that should be collected by collectstatic.
 
 from django.contrib.staticfiles.finders import BaseFinder
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import sass
 
 
 class SimpleBulmaFinder(BaseFinder):
@@ -17,6 +19,43 @@ class SimpleBulmaFinder(BaseFinder):
 
     storage_class = FileSystemStorage
 
+    def _get_bulma_css(self):
+        """
+        Build and compile the bulma css file.
+        """
+
+        # Start by unpacking the users custom variables
+        bulma_settings = settings.BULMA_SETTINGS
+        scss_string = ""
+        for var, value in bulma_settings.get("variables", []):
+            scss_string += f"${var}: {value}\n"
+
+        # Now load in the extensions that the user wants
+        extensions = bulma_settings.get("extensions", True)
+
+        if extensions is True:
+            scss_string += '@import "sass/extensions/_all"'
+        elif isinstance(extensions, list):
+            for extension in extensions:
+                scss_string += f'@import "sass/extensions/{extension}"'
+
+        # Finally, load bulma
+        scss_string += '@import "bulma"'
+
+        # Store this as a css file
+        css_string = sass.compile(string=scss_string)
+        # Save to file
+
+        return  # The file
+
+    def _get_bulma_js(self):
+        """
+        Returns a list of all the js files that are
+        needed for the users selected extensions.
+        """
+
+        pass
+
     def find(self, path, all=False):
         """
         Given a relative file path, find an absolute file path.
@@ -24,6 +63,7 @@ class SimpleBulmaFinder(BaseFinder):
         If the ``all`` parameter is False (default) return only the first found
         file path; if True, return a list of all found files paths.
         """
+
         raise NotImplementedError("subclasses of BaseFinder must provide a find() method")
 
     def list(self, ignore_patterns):
@@ -31,4 +71,5 @@ class SimpleBulmaFinder(BaseFinder):
         Given an optional list of paths to ignore, return a two item iterable
         consisting of the relative path and storage instance.
         """
-        raise NotImplementedError("subclasses of BaseFinder must provide a list() method")
+
+        pass
