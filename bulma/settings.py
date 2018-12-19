5d67c0d72dbff29f07d37173f2cbfb70a093bf07
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-19 12:46:19 +0100

    removing unneeded stuff from settings.py to prevent confusion

--- bulma/settings.py
+++ bulma/settings.py
@@ -4,17 +4,4 @@ pdoc will fail with django.core.exceptions.ImproperlyConfigured:
 Requested settings, but settings are not configured.
 """
 
-SECRET_KEY = "It is very stupid that I need to create this file just to get pdoc working"
-DEBUG = True
-
-# Customizing Bulma
-BULMA_SETTINGS = {
-    "extensions": [
-        "bulma-accordion",
-        "bulma-calendar"
-    ],
-    "variables": {
-        "primary": "#000000",
-        "size-1": "6rem"
-    }
-}
+SECRET_KEY = "I'm sure there's a better way to do this..."
