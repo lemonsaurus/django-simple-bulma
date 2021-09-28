d319f630085711a9ff911cb764c70079a8d4d6ba
Ceres <Ceres445@users.noreply.github.com>
2021-09-28 15:24:06 +0530

    Added verbose error

--- django_simple_bulma/extensions/bulma-collapsible-runner/dist/js/bulma-collapsible-runner.js
+++ django_simple_bulma/extensions/bulma-collapsible-runner/dist/js/bulma-collapsible-runner.js
@@ -3,5 +3,10 @@
 // Documentation: https://bulma-collapsible.netlify.app/javascript/
 
 window.addEventListener("load", () => {
+  if (typeof bulmaCollapsible === "undefined") {
+    throw new Error(
+      "The bulma-collapsible extension is not enabled, enable it to use the runner."
+    );
+  }
   bulmaCollapsible.attach();
 });
