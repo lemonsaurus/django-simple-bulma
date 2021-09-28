a19004da3347547822cf806be0b8fec6ce3ee492
Ceres <Ceres445@users.noreply.github.com>
2021-09-28 13:54:17 +0530

    Added runner file to load collapsible elements

---
+++ django_simple_bulma/extensions/bulma-collapsible-runner/dist/bulma-collapsible-runner.js
@@ -0,0 +1,7 @@
+// Makes bulma-collapsible work out of the box
+// Needs the bulma-collapsible extension enabled to work
+// Documentation: https://bulma-collapsible.netlify.app/javascript/
+
+window.addEventListener("load", () => {
+  bulmaCollapsible.attach();
+});
