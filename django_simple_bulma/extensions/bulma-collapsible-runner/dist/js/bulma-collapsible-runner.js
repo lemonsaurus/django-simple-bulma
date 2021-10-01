84f662b70224a001fccc060b9674c14a205b280a
Ceres <42400528+Ceres445@users.noreply.github.com>
2021-10-01 23:08:44 +0530

    Added back verbose error
    Some mishaps made me force push and remove the commit, added it back

--- django_simple_bulma/extensions/bulma-collapsible-runner/dist/js/bulma-collapsible-runner.js
+++ django_simple_bulma/extensions/bulma-collapsible-runner/dist/js/bulma-collapsible-runner.js
@@ -1,7 +1,10 @@
-// Makes bulma-collapsible work out of the box
-// Needs the bulma-collapsible extension enabled to work
 // Documentation: https://bulma-collapsible.netlify.app/javascript/
 
 window.addEventListener("load", () => {
+  if (typeof bulmaCollapsible === "undefined") {
+    throw new Error(
+      "The bulma-collapsible extension is not enabled, enable it to use the runner."
+    );
+  }
   bulmaCollapsible.attach();
 });
